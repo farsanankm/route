3b2e70fbcf5d449b334465d12ea2a983e81fbf39
farsana nkm <fasrsanankm24@gmail.comgit config --global user.email fasrsanagit config --global user.email fasrsanankm24@gmail.com>
2024-05-22 13:56:55 +0530

    sd

--- src/About.js
+++ src/About.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import './About.css'
+import pho from '../src/images/7712573.png'
 
 function About() {
   return (
     <div className="u ">
 
-      <div className="q">
-      <p><h1 style={{fontFamily:"-moz-initial"}}>DO YOU KNOW WHO AM I?....</h1><h2 style={{fontFamily:"serif"}}> HEYYY EVERYONE....</h2></p>
-    <p>IAM FARSANA ABDUL LATHEEF FROM KERALA,INDIA.
-    IAM CURRENTLY PERFORMING AN INTERSHIP IN SOFTCRONICS.
-    I HAVE RECENTLY COMPLETED MY DEGREE FROM EMEA ARTS AND SCIENCE COLLAGE KONDOTTY IN ENGLISH LANGUAGE AND LITERATUFRE.
-    I TOOK A 6 MONTHS BREAK FROM MY ACADEMIC STREAM TO MAKE A SELF STUDY ABOUT MY IT MIND AND THIS LEAD ME TO BE A IT PROFFESIONAL</p>
-      </div>
+      <div className="a1">
+        <div className="q">
+        <p><h1 style={{fontFamily:"-moz-initial",textAlign:"center",paddingRight:"100px"}}>DO YOU KNOW WHO AM I?....</h1></p>
+       <p style={{paddingRight:"100px"}}>HEYYY EVERYONE....IAM FARSANA ABDUL LATHEEF FROM KERALA,INDIA.
+       IAM CURRENTLY PERFORMING AN INTERSHIP IN SOFTCRONICS.
+       I HAVE RECENTLY COMPLETED MY DEGREE FROM EMEA ARTS AND SCIENCE COLLAGE KONDOTTY IN ENGLISH LANGUAGE AND LITERATUFRE.
+       I TOOK A 6 MONTHS BREAK FROM MY ACADEMIC STREAM TO MAKE A SELF STUDY ABOUT MY IT MIND AND THIS LEAD ME TO BE A IT PROFFESIONAL</p>
+       <p>
+       <ul >
+        <h2 style={{paddingTop:"30px", paddingBottom:"20px"}}>Apart from coding, some other activities that I love to do!</h2>
+        <li style={{fontSize:"130%"}}>Travelling</li>
+        <li  style={{fontSize:"130%"}}>Entertaining</li>
+        <li  style={{fontSize:"130%"}}>Creating personal websites and blogs</li>
+       </ul>
        
+       </p>
+        </div>
+        <div className='ai'>
+          <img  src={pho} width={"400px"} style={{float:"right"}}/>
+        </div>
+      
+      </div>
+      <div className='aa'>
+
+      </div>
+       <div className='as'>
+
+       </div>
     </div>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -22,13 +22,17 @@ function BasicExample() {
             
                 
               <Link style={{textDecoration:"none"}} to='/home'>
-                <h1 style={{fontSize:"20px",color:'black',fontFamily:"serif"}}>Home</h1>
+                <h1 style={{fontSize:"20px",color:'black',fontFamily:"serif"}}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+                </svg>Home</h1>
               </Link>
                
             
                
                <Link style={{textDecoration:"none"}} to='/ABOUT'>
-                <h1 style={{fontSize:"20px",color:'black',fontFamily:"serif"}}>About</h1>
+                <h1 style={{fontSize:"20px",color:'black',fontFamily:"serif"}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z"/>
+                </svg>About</h1>
               </Link>
                
               
@@ -36,26 +40,37 @@ function BasicExample() {
 
              
              <Link  style={{textDecoration:"none"}}to='/Qualification'>
-                <h1 style={{fontSize:"20px",color:'black',fontFamily:"serif"}}>Qualification</h1>
+                <h1 style={{fontSize:"20px",color:'black',fontFamily:"serif"}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-buildings" viewBox="0 0 16 16">
+                <path d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022M6 8.694 1 10.36V15h5zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5z"/>
+                 <path d="M2 11h1v1H2zm2 0h1v1H4zm-2 2h1v1H2zm2 0h1v1H4zm4-4h1v1H8zm2 0h1v1h-1zm-2 2h1v1H8zm2 0h1v1h-1zm2-2h1v1h-1zm0 2h1v1h-1zM8 7h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zM8 5h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zm0-2h1v1h-1z"/>
+                </svg>Qualification</h1>
               </Link>
 
              
 
             
               <Link style={{textDecoration:"none"}} to='/Skills'>
-                <h1 style={{fontSize:"20px",color:'black',fontFamily:"serif"}}>Skills</h1>
+                <h1 style={{fontSize:"20px",color:'black',fontFamily:"serif"}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                </svg>Skills</h1>
               </Link>
              
 
 
             
              <Link style={{textDecoration:"none"}}to='/Contact'>
-                <h1 style={{fontSize:"20px", color:'black',fontFamily:"serif"}}>Contact</h1>
+                <h1 style={{fontSize:"20px", color:'black',fontFamily:"serif"}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                </svg>Contact</h1>
               </Link>
 
             
              <Link style={{textDecoration:"none"}} to='/Resume'>
-                <h1 style={{fontSize:"20px", color:'black',fontFamily:"serif"}}>Resume</h1>
+                <h1 style={{fontSize:"20px", color:'black',fontFamily:"serif"}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-text" viewBox="0 0 16 16">
+                <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
+                 <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
+                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/>
+                </svg>Resume</h1>
               
               </Link>
              
